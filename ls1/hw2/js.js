3b2a1c132f373380806105c4051b288d87396e3a
Enalivayko <evg.nalivayko@gmail.com>
2023-02-28 17:43:03 +0300

    hw 1-3 r

--- ls1/hw2/js.js
+++ ls1/hw2/js.js
@@ -3,9 +3,9 @@
 let a = Number(prompt('Enter the first number',''));
 let b = Number(prompt('Enter the second number',''));
 
-console.log(a + b);
-console.log(a - b);
-console.log(a / b);
-console.log(a * b);
+console.log(`Результат: ${a} + ${b} =` ,a + b);
+console.log(`Результат: ${a} - ${b} =` ,a - b);
+console.log(`Результат: ${a} / ${b} =` ,a / b);
+console.log(`Результат: ${a} * ${b} =` ,a * b);
 
 
