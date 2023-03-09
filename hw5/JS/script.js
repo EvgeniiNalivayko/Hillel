0d74c9ac4bc5d60aece7f4adc25b4fae4a584689
Enalivayko <evg.nalivayko@gmail.com>
2023-03-09 14:00:27 +0300

    hw 5 replace let with const where possible and remove unnecessary empty lines.

--- hw5/JS/script.js
+++ hw5/JS/script.js
@@ -1,16 +1,13 @@
 //Task #1
-
-let user = {}
+const user = {}
 user.name = 'John';
 user.surname = 'Smith';
 
 user.name = 'Pete'
-
 delete user.name;
 
 //Task #2
 //Можно ли изменить объект, объявленный с помощью const?
-
 // это будет работать?
 //user.name = «Pete»;
 //---------------------------------------------
@@ -18,25 +15,22 @@ delete user.name;
 //Когда мы изменяем имя объекта это все равно что создать новый или переназначаем его. Это можно было сделать, если
 //вместо const будет let.
 
-
 //Task #3
-
 function sumAllNumberValue(obj) {
     let sum = 0;
-    for (const name in obj) {
-        if (typeof obj[name] === 'number') {
-            sum += obj[name]
+    for (const k in obj) {
+        if (typeof obj[k] === 'number') {
+            sum += obj[k];
         }
     }
-    return sum
-
+    return sum;
 }
 
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+};
 
-let salaries = {
-
-    John: 100, Ann: 160, Pete: 130
-}
-
-let allSalaries = sumAllNumberValue(salaries)
-console.log(`Нужно выплатить: ${allSalaries}`)
+const allSalaries = sumAllNumberValue(salaries);
+console.log(`Нужно выплатить: ${allSalaries}`);
