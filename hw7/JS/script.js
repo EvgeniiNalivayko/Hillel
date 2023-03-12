69d4c2ee6e97e58162f166b3c4dad632833ec673
Enalivayko <evg.nalivayko@gmail.com>
2023-03-12 23:08:18 +0200

    hw7

--- hw7/JS/script.js
+++ hw7/JS/script.js
@@ -1,11 +1,11 @@
 'use strict';
-function foo(a, b) {
+function isNumbersEqual(a, b) {
    return a === b;
 }
 function foo1 (a,b ) {
     return (a + b) > 10;
 }
 
-function foo2(a) {
-    return a < 0;
+function negativeNum(num) {
+    return num < 0;
 }
