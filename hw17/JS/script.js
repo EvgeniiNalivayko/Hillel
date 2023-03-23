e97a0007273240cb8226c7b78d7f5394410a000a
Enalivayko <evg.nalivayko@gmail.com>
2023-03-23 16:19:18 +0200

    Исправил ошибки

--- hw17/JS/script.js
+++ hw17/JS/script.js
@@ -1,23 +1,22 @@
 'use strict'
-const padString = (str, num, sym = '', bool = true) => {
+const padString = (str, num, sym = ' ', bool = true) => {
     if (typeof str !== 'string') throw new Error('error string');
-    if (isNaN(num) || num !== 'number') throw new Error('error number');
-    if (typeof sym !== 'string' || sym.length !== 1 ) throw new Error('error symbol');
+    if (typeof num !== 'number') throw new Error('error number');
+    if (typeof sym !== 'string' || sym.length !== 1) throw new Error('error symbol');
 
     let result = str;
 
-    if (str.length < num && bool) {
-        for (let i = 0; i <= (num - str.length); i++) {
-            result += sym;
-        }
-    } else if (str.length < num && !bool) {
-        for (let i = 0; i <= (num - str.length); i++) {
-            result = sym + str;
-        }
+    if (str.length < num) {
+        const diff = num - str.length;
+
+        if (bool) result = str + sym.repeat(diff);
+        else result = sym.repeat(diff) + str;
     } else {
         result = str.substring(0, num);
     }
-    return result;
-}
 
-console.log(padString('hello', 12, '*',false));
+    return result;
+};
+
+console.log(padString('hello', 12, '*', false));
+
