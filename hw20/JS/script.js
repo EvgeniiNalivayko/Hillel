1ad2b561b814fdb4e0a7d906affbf7b8ae316512
Enalivayko <evg.nalivayko@gmail.com>
2023-03-23 16:23:03 +0200

    Добавил параметр по умолчанию

--- hw20/JS/script.js
+++ hw20/JS/script.js
@@ -1,17 +1,6 @@
 'use strict';
 
-// let fib = (n = 1) => {
-//     let arr = [];
-//     for (let i = 0; i <= 100; i++) {
-//         n = n[i] + n[i - 1];
-//         arr.push(arr[n]);
-//     }
-//     return arr;
-// }
-//
-// console.log(fib);
-
-let fib = (n) => {
+let fib = (n = 1) => {
     let prevNum = 0;
     let currNum = 1;
     let i = 0;
@@ -26,7 +15,3 @@ let fib = (n) => {
 }
 fib(10)
 
-// function fib(n) {
-//     if (n <= 1) return n;
-//     else return fib(n - 1) + fib(n - 2);
-// }
