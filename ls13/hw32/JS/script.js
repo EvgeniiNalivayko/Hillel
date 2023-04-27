290c6c916ea5aaddde57d4d28ba539f216858efb
Enalivayko <evg.nalivayko@gmail.com>
2023-04-27 12:45:47 +0300

    hw32

--- ls13/hw32/JS/script.js
+++ ls13/hw32/JS/script.js
@@ -1,4 +1,14 @@
 'use strict';
-(function (){
-
+(function () {
+    const arr4 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+    const myFlat = (arr) => {
+        const newArr = [];
+        const flatten = arr => {
+            arr.forEach(el => (Array.isArray(el) ? flatten(el) : newArr.push(el)));
+        }
+        flatten(arr);
+        return newArr;
+    }
+    const flat = myFlat(arr4);
+    console.log(flat);
 })()
