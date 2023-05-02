a506f7cf8d06cb2145faa1c20e58d7cce567fe52
Enalivayko <evg.nalivayko@gmail.com>
2023-05-02 19:09:03 +0300

    hw18

--- hw18/JS/script.js
+++ hw18/JS/script.js
@@ -1,5 +1,5 @@
 const arrN = [1, 2, 3, 2, -2, -3, 5, 5, 23, 23, -42];
-let positiveNumbersFilter = (arr) => {
+const positiveNumbersFilter = (arr) => {
     if (!Array.isArray(arr)) return "Переданный параметр не является массивом";
 
     const positiveArr = [];
