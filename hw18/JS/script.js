89f84a043fda2e96ea79e63c35890385cc78bb4e
Enalivayko <evg.nalivayko@gmail.com>
2023-05-02 19:08:35 +0300

    hw18

--- hw18/JS/script.js
+++ hw18/JS/script.js
@@ -1,20 +1,20 @@
-const arrN = [1, 2, 3, 2, -2, -3, 5, 5, 23, 23, -42]
+const arrN = [1, 2, 3, 2, -2, -3, 5, 5, 23, 23, -42];
 let positiveNumbersFilter = (arr) => {
     if (!Array.isArray(arr)) return "Переданный параметр не является массивом";
 
-    const positiveArr = []
+    const positiveArr = [];
 
     if (arr.length === 0) {
         console.log('Массив пустой');
     }
 
-    if (!arr.every((item) => typeof item === 'number')) return 'Проверка исключительно чисел'
-    for (let i = 0; i <= arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         if (arr[i] > 0) {
             positiveArr.push(arr[i]);
         }
     }
-    return positiveArr.length > 0 ? positiveArr : null
+
+    return positiveArr;
 }
 console.log(positiveNumbersFilter(arrN));
 
