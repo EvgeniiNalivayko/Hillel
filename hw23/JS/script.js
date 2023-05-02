301f46578d3efd1c66d2ded6e9959f1557d6ff53
Enalivayko <evg.nalivayko@gmail.com>
2023-05-02 19:02:20 +0300

    hw23

--- hw23/JS/script.js
+++ hw23/JS/script.js
@@ -1,45 +1,32 @@
 const mainFunction = (callback) => {
-    let number;
-    let expon;
-    for (; ;) {
-        number = prompt('Введите число:');
-        if (!isNaN(number) && number !== null) {
-            break;
-        }
-    }
-
-    for (; ;) {
-        expon = prompt('Введите степень числа:');
-        if (!isNaN(expon) && expon !== null) {
-            break;
-        }
-    }
-    callback(+number, +expon);
-
+    let number1 = +prompt("Введите первое число");
+    let number2 = +prompt("Введите второе число");
+    if (number1 === null || !number1 || number2 === null || !number2) throw new Error('Введите число')
+    callback(number1, number2);
 }
 
-const exponentiation = (number, expon) => {
+const exponentiation = (number, number2) => {
     const operator = '**';
-    const result = number ** expon;
-    alert(`${number} ${operator} ${expon} = ${result}`);
+    const result = number ** number2;
+    alert(`${number} ${operator} ${number2} = ${result}`);
     return result;
 }
-const multiplay = (number, expon) => {
+const multiplay = (number, number2) => {
     const operator = '*';
-    const result = number * expon;
-    alert(`${number} ${operator} ${expon} = ${result}`);
+    const result = number * number2;
+    alert(`${number} ${operator} ${number2} = ${result}`);
     return result;
 }
-const division = (number, expon) => {
+const division = (number, number2) => {
     const operator = '/';
-    const result = number / expon;
-    alert(`${number} ${operator} ${expon} = ${result}`);
+    const result = number / number2;
+    alert(`${number} ${operator} ${number2} = ${result}`);
     return result;
 }
-const modulo = (number, expon) => {
+const modulo = (number, number2) => {
     const operator = '%';
-    const result = number % expon;
-    alert(`${number} ${operator} ${expon} = ${result}`);
+    const result = number % number2;
+    alert(`${number} ${operator} ${number2} = ${result}`);
     return result;
 }
 
