6d83c5ae30e44a30b523335bb3715f5344030364
Enalivayko <evg.nalivayko@gmail.com>
2023-06-25 13:53:10 +0300

    hw64

--- hw64/JS/script.js
+++ hw64/JS/script.js
@@ -4,50 +4,48 @@
     function Student(firstName, lastName, birthYear) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.birthYear = birthYear;
+        this.birthYear = +birthYear;
         this.marksArr = new Array(10).fill(null);
         this.attendanceList = new Array(10).fill(null);
     }
 
     Student.prototype.age = function () {
         const currentYear = new Date().getFullYear();
-        return currentYear - +this.birthYear;
+        return currentYear - this.birthYear;
     };
 
     Student.prototype.gradePointAverage = function () {
         const validGrades = this.marksArr.filter((grade) => grade !== null);
-        if (validGrades.length === 0) return;
         const sumGrades = validGrades.reduce((acc, grade) => acc + grade, 0);
         return sumGrades / validGrades.length;
     };
 
-    Student.prototype.present = function () {
+    Student.prototype.setAttendance = function (flag) {
         if (this.attendanceList.length > 10) throw new Error('Количество занятий ошибочно');
         const emptyIndex = this.attendanceList.findIndex((item) => item === null);
-        if (emptyIndex !== -1) {
-            const attendance = this.attendanceList[emptyIndex] = true;
-            return attendance;
-        }
+        if (emptyIndex !== -1) return this.attendanceList[emptyIndex] = flag;
+    }
+
+    Student.prototype.present = function () {
+        this.setAttendance(true);
     };
 
     Student.prototype.absent = function () {
-        if (this.attendanceList.length > 10) throw new Error('Количество занятий ошибочно');
-        const emptyIndex = this.attendanceList.findIndex((item) => item === null);
-        if (emptyIndex !== -1) return this.attendanceList[emptyIndex] = false;
+        this.setAttendance(false);
     };
 
     Student.prototype.mark = function (mark) {
         if (mark > 10 || mark < 0) throw new Error('Мы учимся по 10бальной системе');
         if (this.attendanceList.length > 10) throw new Error('Количество занятий ошибочно');
-        const presentAttendance = this.attendanceList.filter((item) => item !== null);
-        const lastIndex = presentAttendance.length - 1;
-        return this.marksArr[lastIndex] = mark;
+        const presentAttendanceCount = this.attendanceList.filter((item) => item !== null).length - 1;
+        return this.marksArr[presentAttendanceCount] = mark;
     };
 
     Student.prototype.averageAttendance = function () {
         const attendanceCount = this.attendanceList.filter((item) => item !== null).length;
         const sumOfAttendance = this.attendanceList.reduce((acc, item) => acc + item, 0);
-        return sumOfAttendance / attendanceCount;
+        return sumOfAttendance / attendanceCount
+
     }
 
     Student.prototype.summary = function () {
@@ -64,10 +62,9 @@
 
     const student = new Student('Петя', 'Петров', 1999);
     student.present();
-    student.mark(7);
+    student.mark(10);
     student.present();
     student.present();
-    student.absent();
     student.present();
     student.mark(10);
     const summary = student.summary();
