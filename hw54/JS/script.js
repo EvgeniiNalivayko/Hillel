cb253839010851232a302c57cf740e305605e4af
Enalivayko <evg.nalivayko@gmail.com>
2023-06-28 13:57:52 +0300

    hw54

---
+++ hw54/JS/script.js
@@ -0,0 +1,26 @@
+'use script';
+const user = {
+    data: {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: {
+            a1: 1,
+            b1: 2,
+            c1: 3,
+            d1: {
+                a2: 3,
+                b2: 3,
+                c2: 3,
+            }
+        },
+    }
+};
+
+const deepFreeze = obj => {
+    Object.freeze(obj);
+    for (let key in obj) {
+        if (typeof obj[key] === 'object') deepFreeze(obj[key])
+    }
+}
+deepFreeze(user)
