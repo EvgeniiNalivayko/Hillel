859dd6760b164635cdbc29841169e9ba7f86bc0a
Enalivayko <evg.nalivayko@gmail.com>
2023-07-02 13:23:43 +0300

    hw67

--- hw67/JS/script.js
+++ hw67/JS/script.js
@@ -1,14 +1,20 @@
 'use script';
 (function () {
     const goods = [['футболка', 15], ['шорты', 25], ['носки', 5]];
+    const priceMap = new Map()
     const getTotalCost = arr => {
-        const map = new Map(arr)
+
         let totalCost = 0;
-        for (let price of map.values()) {
+
+        for (let [item, price] of arr) {
+            priceMap.set(item, price);
+        }
+
+        for (let price of priceMap.values()) {
             totalCost += price;
         }
         return totalCost;
     }
-    console.log(getTotalCost(goods));
+    console.log(getTotalCost(goods));//45
 
 })()
