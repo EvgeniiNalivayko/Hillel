336590ed7848b1018e9cb482d1fb5e802c1e6431
Enalivayko <evg.nalivayko@gmail.com>
2023-07-02 13:18:43 +0300

    hw68

--- hw68/JS/script.js
+++ hw68/JS/script.js
@@ -1,13 +1,12 @@
 'use strict';
 (function () {
     const str = "abracadabra";
+    const unique = new Set()
     const uniqueSymbol = str => {
-        const unique = new Set()
 
         for (let item of str) {
             unique.add(item)
         }
-
         return unique.size;
     }
     console.log(uniqueSymbol(str));
